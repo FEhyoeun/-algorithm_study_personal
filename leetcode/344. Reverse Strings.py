3c6d01ef8f1ac9d6b81bfc0c18516b1f24361fc7
FEhyoeun <khyoeun92@gmail.com>
2022-06-26 12:44:44 +0900

    [리트코드]Reverse Strings  - 파이썬

---
+++ leetcode/344. Reverse Strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        reversed_word = ''.join(s[::-1])
+
+        for i in reversed_word:
+            s.append(i)
+            s.pop(0)
+        
